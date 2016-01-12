6232bcb025e042c840b37566d14700011f6f9424
bjuppa <nilsved@gmail.com>
2016-01-12 17:09:34 +0100

    fixed content assertion

--- src/Testing/ComparesFluentHtml.php
+++ src/Testing/ComparesFluentHtml.php
@@ -29,7 +29,7 @@ trait ComparesFluentHtml
     {
         $e->withHtmlElementName(null); //Removing the tag name makes the element hidden in html
 
-        static::assertEquals(static::comparableHtml($expected), static::comparableHtml($e),
+        static::assertEquals(static::comparableHtml($expected), static::comparableHtml($e->toHtml()),
             $message ?: 'FluentHtml contents not matching HTML string');
     }
 }
