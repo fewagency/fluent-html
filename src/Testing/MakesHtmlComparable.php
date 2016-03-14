409d4678a1fe2db2ce1f964041eb139fcbaa6b87
bjuppa <nilsved@gmail.com>
2016-03-14 13:06:22 +0100

    made html more comparable by splitting >< to > <

--- src/Testing/MakesHtmlComparable.php
+++ src/Testing/MakesHtmlComparable.php
@@ -4,12 +4,35 @@ namespace FewAgency\FluentHtml\Testing;
 trait MakesHtmlComparable
 {
     /**
-     * Replaces any whitespace characters with a single space
+     * Makes an html string comparable.
      * @param $html_string
      * @return string
      */
     protected static function comparableHtml($html_string)
     {
+        $html_string = self::whitespaceBetweenHtmlTags($html_string);
+        $html_string = self::uniformHtmlWhitespace($html_string);
+
+        return $html_string;
+    }
+
+    /**
+     * Replaces any whitespace characters with a single space.
+     * @param $html_string
+     * @return mixed
+     */
+    protected static function uniformHtmlWhitespace($html_string)
+    {
         return preg_replace('/\s+/', ' ', $html_string);
+    }
+
+    /**
+     * Inserts whitespace between any adjacent html tags.
+     * @param $html_string
+     * @return mixed
+     */
+    protected static function whitespaceBetweenHtmlTags($html_string)
+    {
+        return preg_replace('/></', '> <', $html_string);
     }
 }
